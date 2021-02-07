2e490ab33d84593aa2eb9039fab948e4fdd20de2
Charlie Harding <charlie_harding@icloud.com>
2021-02-07 21:28:42 +0100

    Add tests and coverage to CI

--- model/jest.config.js
+++ model/jest.config.js
@@ -1,5 +1,8 @@
 /* eslint-env node */
 module.exports = {
+  coverageDirectory: "./.coverage/",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts"],
   testRegex: "/test/.*\\.spec\\.ts",
   preset: "ts-jest",
   testEnvironment: "node",
