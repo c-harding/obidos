7fc9f8d8973783a4b58873e8599f9964ff132c08
Charlie Harding <charlie_harding@icloud.com>
2021-02-21 15:16:17 +0100

    Correct format of Octokit annotations

--- actions/process-jest-json.ts
+++ actions/process-jest-json.ts
@@ -16,6 +16,12 @@ type Annotation = {
   title?: string | undefined;
 };
 
+type KeysOfType<T, TProp> = NonNullable<
+  {
+    [P in keyof T]: T[P] extends TProp ? P : never;
+  }[keyof T]
+>;
+
 function getAnnotations(results: FormattedTestResults, cwd: string): Annotation[] {
   return results.testResults.flatMap((result) =>
     result.assertionResults
@@ -52,6 +58,14 @@ function getSha(): string {
   return context.payload.pull_request?.head.sha ?? context.sha;
 }
 
+function getOutputText(results: FormattedTestResults[]) {
+  const entries = results
+    .flatMap((result) => result.testResults)
+    .map((r) => r.message)
+    .filter(Boolean);
+  return "```\n" + entries.join("\n").trimRight() + "\n```";
+}
+
 async function main([, , ...paths]: string[]): Promise<void> {
   try {
     const token = process.env.GITHUB_TOKEN;
@@ -69,9 +83,28 @@ async function main([, , ...paths]: string[]): Promise<void> {
 
     const octokit = getOctokit(token);
 
-    const success = Promise.all(jestFilePromises).then((array) =>
-      array.every((jestFile) => jestFile.success),
-    );
+    const jestFiles = await Promise.all(jestFilePromises);
+
+    const success = jestFiles.every((file) => file.success);
+    type SummaryKey = KeysOfType<FormattedTestResults, number>;
+    const summaryKeys: SummaryKey[] = [
+      "numPassedTests",
+      "numPassedTestSuites",
+      "numFailedTests",
+      "numFailedTestSuites",
+    ];
+    const summary = Object.fromEntries(
+      summaryKeys.map((summaryKey) => [
+        summaryKey,
+        jestFiles.map((jestFile) => jestFile[summaryKey]).reduce((a, b) => a + b, 0),
+      ]),
+    ) as Record<SummaryKey, number>;
+
+    const summaryText = success
+      ? `${summary.numPassedTests} tests passing in ${summary.numPassedTestSuites} suite${
+          summary.numPassedTestSuites > 1 ? "s" : ""
+        }.`
+      : `Failed tests: ${summary.numFailedTests}/${summary.numTotalTests}. Failed suites: ${summary.numFailedTests}/${summary.numTotalTestSuites}.`;
 
     // Checks
     await octokit.checks.create({
@@ -79,8 +112,13 @@ async function main([, , ...paths]: string[]): Promise<void> {
       head_sha: getSha(),
       name: "Test results",
       status: "completed",
-      conclusion: (await success) ? "success" : "failure",
-      output: await annotationsPromise,
+      conclusion: success ? "success" : "failure",
+      output: {
+        title: success ? "Jest tests passed" : "Jest tests failed",
+        text: success ? "" : getOutputText(jestFiles),
+        summary: summaryText,
+        annotations: await annotationsPromise,
+      },
     });
   } catch (error) {
     console.error(error);
